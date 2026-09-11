a33c66b4d106fe3bf265717e55775962ea7d8f4c
JibranAhmed554 <ahmedjibran469@gmail.com>
2026-09-12 03:08:43 +0500

    frist

---
+++ config/db.config.mjs
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDb = async () => {
+  const DB_NAME = "ecommerce_db";
+
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGO_ALTAS_URI}/${DB_NAME}`);
+    console.log(`Connected to: ${conn.connection.host}`);
+    console.log(`Connected database: ${conn.connection.name}`);
+  } catch (error) {
+    console.log(`Error in connection: `, error.message);
+  }
+};
+
+export default connectDb;
